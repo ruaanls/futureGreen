f89479ce588c43cf3791905ed91d2e64fc828af8
Ruan Lima <124522882+ruaanls@users.noreply.github.com>
2024-11-12 22:01:30 -0300

    Limpeza do boyler plate

---
+++ future-green/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+
+export const metadata: Metadata = {
+  title: "Global Solution",
+  description: "Green Future",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
