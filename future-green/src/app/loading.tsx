b7ece9ae8c24507bae3fce1545f02b7e2571a9ad
Ruan Lima <124522882+ruaanls@users.noreply.github.com>
2024-11-20 23:02:55 -0300

    Erros vercel

--- future-green/src/app/loading.tsx
+++ future-green/src/app/loading.tsx
@@ -1,7 +1,7 @@
 import Spinner from '@/components/Spinner/Spinner'
 import React from 'react'
 
-export default function () {
+export default function Loading() {
   return (
     <div>
         <Spinner/>
--- future-green/src/app/not-found.tsx
+++ future-green/src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function notfound() {
+export default function Notfound() {
   return (
     <div className='h-[90vh] bg-[#cccccc] flex items-center justify-center'>
         <h1 className='text-[2rem] font-[500]'>ERRO 404 - PÁGINA NÃO ENCONTRADA</h1>
