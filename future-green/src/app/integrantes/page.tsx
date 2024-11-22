b04e5bdd654dd1afa20f204126208707640fb71c
RichardyBS <editionplays@gmail.com>
2024-11-22 16:42:03 -0300

    Integrantes ajuste

--- future-green/src/app/integrantes/page.tsx
+++ future-green/src/app/integrantes/page.tsx
@@ -10,12 +10,13 @@ export default function Integrantes() {
     return (
         <main>
             <section className="sectionIntegrante">
-                <h1>Integrantes do Projeto</h1>
+                <h1>Turma 1TDSPW</h1>
                 <div className="containerIntegrante">
                 <div className="integranteBox">
                     <Image src={richardybs} className="imgSocial" alt="imagem do integrante Richardy"/>
                     <div className="infoIntegrant">
                     <h2>Richardy Borges</h2>
+                    <h2>rm - 557883</h2>
                     <div className="Socials">
                         <Link href={"https://github.com/richardybs"} target="_blank"><VscGithub/></Link>
                         <Link href={"https://linkedin.com/in/richardybs"} target="_blank"><FaLinkedin/></Link>
@@ -27,9 +28,9 @@ export default function Integrantes() {
                     <Image src={ruanjs} className="imgSocial" alt="imagem do integrante Richardy"/>
                     <div className="infoIntegrant">
                     <h2>Ruan Lima Silva</h2>
+                    <h2>rm - 558775</h2>
                     <div className="Socials">
                         <Link href={"https://github.com/ruaanls"} target="_blank"><VscGithub/></Link>
-                        {/* arruma com base no seu */}
                         <Link href={"https://github.com/ruaanls"} target="_blank"><FaLinkedin/></Link>
                     </div>
                     </div>
@@ -38,10 +39,10 @@ export default function Integrantes() {
                 <div className="integranteBox">
                     <Image src={izzi} className="imgSocial" alt="imagem do integrante Richardy"/>
                     <div className="infoIntegrant">
-                    <h2>Henrique Izzi</h2>
+                    <h2>Henrique Izzi</h2> 
+                    <h2>rm - 555413</h2>
                     <div className="Socials">
                         <Link href={"https://github.com/rickizzisj"} target="_blank"><VscGithub/></Link>
-                        {/* arruma com base no seu */}
                         <Link href={"https://github.com/rickizzisj"} target="_blank"><FaLinkedin/></Link>
                     </div>
                     </div>
